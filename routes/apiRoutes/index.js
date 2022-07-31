b7a3d41abbaa5dc5288a893f021a71f2efe6eb51
enajera1 <eriknajera013@gmail.com>
2022-07-31 13:42:37 -0500

    starter code loaded

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { notes } = require('../../data/db.json');
+const { createNewNote, deleteNote } = require('../../lib/notes');
+
+// route to render 'notes' data
+router.get('/notes', (req, res) => {
+  res.json(notes);
+});
+
+// route to add new data to 'notes'
+router.post('/notes', (req, res) => {
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+
+});
+
+// deletes data from the file
+router.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const removeNote = deleteNote(id, notes);
+  res.json(removeNote);
+})
+
+module.exports = router; 
