4862754f1c561030cc62f896296461f5bf1aac46
enajera1 <eriknajera013@gmail.com>
2022-07-31 14:11:44 -0500

    looking to fix error

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,19 +2,16 @@ const router = require('express').Router();
 const { notes } = require('../../data/db.json');
 const { createNewNote, deleteNote } = require('../../lib/notes');
 
-// route to render 'notes' data
 router.get('/notes', (req, res) => {
   res.json(notes);
 });
 
-// route to add new data to 'notes'
 router.post('/notes', (req, res) => {
   const note = createNewNote(req.body, notes);
   res.json(note);
 
 });
 
-// deletes data from the file
 router.delete('/notes/:id', (req, res) => {
   const id = req.params.id;
   const removeNote = deleteNote(id, notes);
